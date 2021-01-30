fc09061137c3a3c6d4cfdb36baea71075b6fa054
varun0308 <varunsathish038@gmail.com>
2021-01-31 01:54:35 +0530

    commited

---
+++ course/numpy/basic1.py
@@ -0,0 +1,57 @@
+# NumPy used to create n-dimensional arrays
+# Homogenous array (should be all int or all string.....)
+
+import numpy as np
+
+a= np.array(42)
+b= np.array([1,3,2])
+c= np.array([[[1,3,2]]])
+
+# a.ndim gives the dimension of that array
+print(a.ndim,b.ndim,c.ndim)
+
+
+
+
+
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+bool_result = (arr % 2 == 0)
+newarr = arr[bool_result]
+print(bool_result)
+print(newarr)
+
+from numpy import random
+
+x = random.rand()
+print(x)
+
+x = random.randint(10)
+print(x)
+
+x = random.randint(20,size=4)
+print(x)
+
+x = random.choice([3, 5, 7, 9], size=(3, 5))
+print(x)
+
+x = random.choice(20, size=5)
+print(x)
+
+x = random.choice(20, size=(3, 5))
+print(x)
+
+
+
+import pandas as pd
+
+var = pd.Series([1,3,2,6,4,7])
+print(var)
+
+a = [1, 7, 2, 8]
+
+myvar = pd.Series(a, index = ["x", "y", "z", "j"])
+
+print(myvar)
+
+
---
+++ course/numpy/basic2_arange.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+print(np.arange(5))
+
+print(np.arange(2,6))
+
+print(np.arange(2,12,2))
+
+print(np.arange(2.2,9.33))
+
+print(np.arange(2.2,9.3,0.9))
+
+arr = np.arange(10)
+print(arr)
+print(arr[2]+arr[4])
+
+arr1 = np.array([[1,3,2,4],[5,7,6,8]])
+print(arr1[1][3])
